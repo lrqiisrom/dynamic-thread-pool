a96bebcf58b21e84b9f782d4f1610411fffa5ef1
lrqiisrom <2806649895@qq.com>
2025-04-29 23:12:05 +0800

    采集线程池配置数据

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/rom/middleware/dynamic/thread/pool/sdk/domain/IDynamicThreadPoolService.java
@@ -0,0 +1,16 @@
+package com.rom.middleware.dynamic.thread.pool.sdk.domain;
+
+import com.rom.middleware.dynamic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+
+import java.util.List;
+
+/**
+ * 动态线程池服务
+ */
+public interface IDynamicThreadPoolService {
+    List<ThreadPoolConfigEntity> queryThreadPoolList();
+
+    ThreadPoolConfigEntity queryThreadPoolConfigByName(String threadPoolName);
+
+    void updateThreadPoolConfig(ThreadPoolConfigEntity threadPoolConfigEntity);
+}
